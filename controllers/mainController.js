e105d1366539ceb9a6c666806893ca7700222405
galedesma <gonza.a.ledesma@gmail.com>
2021-03-15 13:39:48 -0300

    Finalizo CRUD

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,6 +1,7 @@
 const Stream = require("../models/stream");
 
 module.exports = {
+  //Devuelve todos los streams
   getStreams: (req, res) => {
     const streams = Stream.find()
       .then((streams) => {
@@ -10,14 +11,58 @@ module.exports = {
         console.log(err);
       });
   },
+  //Devuelve un stream en específico
+  getStream: (req, res) => {
+    const stream = Stream.findById(req.params.id)
+      .then((result) => {
+        res.status(200).json({ stream: result });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+  //Crea un nuevo stream
   createStream: (req, res) => {
     const stream = new Stream(req.body);
     console.log("Creando stream", req.body);
 
-    stream.save().then((result) => {
-      res.status(200).json({
-        stream: result,
+    stream
+      .save()
+      .then((result) => {
+        res.status(200).json({
+          stream: result,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
       });
-    });
+  },
+  //Edita un stream en específico
+  editStream: (req, res) => {
+    const stream = Stream.updateOne(
+      { _id: req.params.id },
+      {
+        $set: {
+          title: req.body.title,
+          description: req.body.description,
+        },
+      }
+    )
+      .then((result) => {
+        res.json({ stream: result });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+  //Borra un stream en específico
+  deleteStream: (req, res) => {
+    const stream = Stream.deleteOne({ _id: req.params.id })
+      .then((result) => {
+        res.json({ stream: result });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   },
 };
--- routes/main.js
+++ routes/main.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { getStreams, createStream } = require("../controllers/mainController");
+const {
+  getStreams,
+  getStream,
+  createStream,
+  editStream,
+  deleteStream,
+} = require("../controllers/mainController");
 
 router.get("/", getStreams);
 router.post("/", createStream);
-router.get("/:id", (req, res) => {
-  res.send(req.params.id);
-});
+router.get("/:id", getStream);
+router.patch("/:id", editStream);
+router.delete("/:id", deleteStream);
 
 module.exports = router;
